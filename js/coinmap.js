ba89e797f1bb80adbf968512466158b9dc6a2732
smartmouse <smartmouse@gmail.com>
2019-02-16 12:29:51 +0100

    added loading page

--- js/coinmap.js
+++ js/coinmap.js
@@ -7,6 +7,11 @@ function coinmap() {
 		maxZoom: 18
 	});
 
+	tileOSM.on('tileload', function (e) {
+		$('#loading').css('visibility','hidden');
+		$('#container').css('visibility','visible');
+	});
+
     var tileOSMBlackAndWhite = L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
         maxZoom: 18,
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
